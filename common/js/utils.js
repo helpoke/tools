b83557e3944d152c7be9c8cc7b7e3ac843d6752a
HiPeng <284236294+helpoke@users.noreply.github.com>
2026-08-25 18:33:31 +0800

    内容丰富，工具优化

--- common/js/utils.js
+++ common/js/utils.js
@@ -235,15 +235,17 @@ function showFileSizeToast(file, maxFileSize) {
 
 // ================= info-section 折叠功能 =================
 /**
- * info-section 中的 article 支持点击 header 折叠/展开，默认折叠
+ * info-section 中的 article 支持点击 header 折叠/展开，默认展开
  */
 (function initInfoSectionToggle() {
     function setup() {
         document.querySelectorAll('.info-details > header').forEach(function (header) {
             header.setAttribute('role', 'button');
-            // true表示默认折叠，false表示默认展开
-            header.setAttribute('aria-expanded', 'false');
-            header.parentElement.classList.add('collapsed');
+            // 默认折叠
+            // header.setAttribute('aria-expanded', 'false');
+            // header.parentElement.classList.add('collapsed');
+            // 默认展开
+            header.setAttribute('aria-expanded', 'true');
         });
     }
 
